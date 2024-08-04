59f07e9a3570d7ef1147fea5996e961d4c599e58
Temirlan Akhmat <a.temirl4n@gmail.com>
2024-08-04 15:08:17 +0500

    Added some sort of validators in forms and models

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 
 class PublisherManager(models.Manager):
@@ -12,7 +13,11 @@ class Women(models.Model):
         DRAFT = 0, 'Черновик'
         PUBLISHED = 1, 'Опубликовано'
     title = models.CharField(max_length=255, verbose_name='Заголовок')
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='Слаг')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='Слаг',
+                            validators=[
+                                MinLengthValidator(5, message='Минимум 5 символов'),
+                                MaxLengthValidator(100, message='Максимум 100 символов')
+                            ])
     content = models.TextField(blank=True, verbose_name='Содержимое')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
